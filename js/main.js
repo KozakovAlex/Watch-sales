0d5d4d202592c7f349b1e650695e4a92e1727e43
KozakovAlex <72526330+KozakovAlex@users.noreply.github.com>
2021-05-03 16:08:09 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -63,26 +63,3 @@ catalogButtonFilter.onclick = function() {
 
 
 
-let textSubtitleDescr = document.querySelector('.text-subtitle-descr');
-let textSubtitleTechs = document.querySelector('.text-subtitle-techs');
-let textDescr = document.querySelector('.text-descr');
-let textTechs = document.querySelector('.text-techs');
-
-textSubtitleTechs.onclick = function() {
-  textSubtitleTechs.classList.add('text-subtitle-active');
-  textSubtitleDescr.classList.remove('text-subtitle-active');
-  textDescr.classList.toggle('js-hidden');
-  textTechs.classList.toggle('js-hidden');
-  // textDescr.style.display = 'none';
-  // textTechs.style.display = 'block';
-}
-
-textSubtitleDescr.onclick = function() {
-  textSubtitleTechs.classList.remove('text-subtitle-active');
-  textSubtitleDescr.classList.add('text-subtitle-active');
-  textDescr.classList.toggle('js-hidden');
-  textTechs.classList.toggle('js-hidden');
-  // textTechs.style.display = 'none';
-  // textDescr.style.display = 'block';
-}
-
